93caf669f0f9b7706c2d1890fa2d248476c74a53
parvvaresh <parvvaresh@gmail.com>
2024-06-04 21:32:23 +0330

    add logstic regression

--- model/Classification/logstic_regression.py
+++ model/Classification/logstic_regression.py
@@ -1,47 +1,55 @@
 import numpy as np
+from .tool import sigmoid
 
-from tool import sigmoid
+class logstic_regression:
+  def __init__(self,
+               max_iters = 10000,
+               learning_rate = 10e-4) -> None:
+    self.max_iters = max_iters
+    self.learning_rate = learning_rate
 
-class logstic_regression: #this for bianary classifiction
-    def __init__(self,
-                 learning_rate : float = 1e-5,
-                 iter : int = 1000) -> None:
-        self.learning_rate = learning_rate
-        self.iter = iter 
-        
-        self.costs = list()
-        self.wieghts = None
-        self.bias = None
+    self.weights = None
+    self.bias = None
+
+  def fit(self,
+          X_train : np.array,
+          y_train : np.array) -> None:
     
-    
-    def fit(self,
-            X_train : np.array,
-            y_train : np.array) -> None:
-        
-        self.n_sampels, self.n_featues = X_train.shape
-        self.wieghts = np.zeros(self.n_featues)
-        self.bias = 0
-        
-        self.costs = []
-        for _ in range(self.iter):
-            y_pred = np.dot(X_train, self.wieghts) + self.bias
-            y_pred_sign = sigmoid(y_pred)
-            
-            loss = -()
-            cost = np.sum(loss) / (2 * self.n_sampels)
-            self.costs.append(cost)
-            
-            dw = None
-            db = None
-            
-            
-            self.wieghts -= (dw * self.learning_rate)
-            self.bias -= (db * self.learning_rate)
-    
-    
-    def predict(self, 
-                X_test : np.array) -> np.array:
-        pass
-            
-        
-                
+    """
+      X_train - > (n, m)
+      wights -‌> (1, m)
+      y_train -> (n , 1)
+      y_predict - > (n , 1)
+      loss ->‌ (n , 1)
+      dw ->‌(1, m)
+    """
+    n_sampels , n_features = X_train.shape
+
+    y_train = y_train.reshape((n_sampels, 1))
+
+    self.weights = np.zeros((1 , n_features))
+    self.bias = 0
+
+    for _ in range(self.max_iters):
+      y_linear = np.dot(X_train, self.weights.T) + self.bias
+      y_predict = sigmoid(y_linear)
+
+      loss = (y_predict - y_train)
+
+      dw = np.dot(loss.T , X_train) / n_sampels
+      db = np.sum(loss) / n_sampels
+
+
+      self.weights -= dw * self.learning_rate
+      self.bias -= db * self.learning_rate
+
+
+
+  def predict(self,
+              X_test : np.array) -> np.array:
+    y_linear = np.dot(X_test, self.weights.T) + self.bias
+    y_predict = sigmoid(y_linear)
+    y_predict_cls = [1 if  pred >= 0.5 else 0 for pred in y_predict]
+    return np.array(
+        y_predict_cls
+    )
