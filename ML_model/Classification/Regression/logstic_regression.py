22db0bd670d798ba951346e7661fd19277ec9e01
reza <parvvaresh@gmail.com>
2024-08-10 20:31:35 +0330

    add logstic regression

--- ML_model/Classification/Regression/logstic_regression.py
+++ ML_model/Classification/Regression/logstic_regression.py
@@ -1,22 +1,48 @@
 import numpy as np
 from Activator_function import sigmoid
 
-class LogsticRregression:
-    def __init__(self, iter = 1000, learning_rate = 1e-5) -> None:
-        self.learning_rate = learning_rate
-        self.iter = iter
+class Logsticregression:
+    def __init__(self, learning_rate : float, n_iters : int) -> None:
+        self.lr = learning_rate
+        self.n_iters = n_iters
         self.weights = None
         self.bias = None
+        self.costs = None
+    
 
     def fit(self, X : np.array, y : np.array) -> None:
         n_samples , n_features = X.shape
+        self.costs = []
 
-        self.weights = np.zeros(n_features)
+        self.weights = np.zeros(n_samples)
         self.bias = 0
 
-        for _ in range(self.iter):
-            linear_model = np.dot(X, self.weights) + self.bias
-
+        for _ in range(self.n_iters):
+            linear_model = np.dot(X, self.weights) +  self.bias
             y_predict = sigmoid(linear_model)
-            #pass
+
+
+            self.costs.append(self._compute_cost(y , y_predict))
+            error = y_predict - y
+
+            dw = np.dot(X.T , error) / (n_samples)
+            db = np.sum(error) / (n_samples)
+
+
+            self.weights -= self.lr * dw
+            self.bias -= self.lr * db
+
+
+    def predict(self, X : np.array) -> np.array:
+        linear_model = np.dot(X.T , self.weights) + self.bias
+        y_predict = sigmoid(linear_model)
+        y_predict = [ 1 if y_pred > 0.5 else 0 for y_pred in y_predict]
+        return np.array(y_predict)
+
+    def _compute_cost(self, y : np.array, y_pred : np.array) -> np.float_:
+        n_sampels = len(y)
+        
+        y_pred = np.clip(y_pred, 1e-10, 1 - 1e-10)
+        cost = -(np.sum(y * np.log(y_pred) + (1 - y) * np.log(1 - y_pred))) / (n_sampels)
+        return cost
 
